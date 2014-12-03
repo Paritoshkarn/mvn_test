77a5bdc1473387bd174359a71b148c7d1dfd86d5
paritosh.a.kumar <paritosh.a.kumar@accenture.com>
2014-12-03 17:57:20 +0530

    changed to run java

--- src/shell/hello.sh
+++ src/shell/hello.sh
@@ -1,7 +1,5 @@
-chmod 777
-
 echo `date +%R` "Message :::: This is purely testing"
 
 export CLASSPATH=:/home/ubuntu/test/final/assembly-1.0-SNAPSHOT/lib/mvn_test-1.0-SNAPSHOT.jar
 
-java -cp $CLASSPATH -jar mvn_test-1.0-SNAPSHOT.jar > out.txt
+java -cp $CLASSPATH com.test.App > out.txt
