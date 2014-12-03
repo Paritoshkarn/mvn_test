9b2a48aaae2272b94e2472a3d252e50fcf2300ed
paritosh.a.kumar <paritosh.a.kumar@accenture.com>
2014-12-03 18:59:04 +0530

    test

--- src/shell/hello.sh
+++ src/shell/hello.sh
@@ -1,5 +1,5 @@
 echo `date +%R` "Message :::: This is purely testing"
 
-export CLASSPATH=:/home/ubuntu/test/final/assembly-1.0-SNAPSHOT/lib/mvn_test-1.0-SNAPSHOT.jar
+export CLASSPATH=:../lib/mvn_test-1.0-SNAPSHOT.jar
 
 java -cp $CLASSPATH com.test.App > out.txt
